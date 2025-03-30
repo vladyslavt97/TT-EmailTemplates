9e9b3822d22e534d194727e58f426964a4e3f57a
Tsurkanenko Vladyslav <VTsurkanenko@stroeer.de>
2025-03-30 16:40:06 +0200

    spinner for when I am back

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,36 +2,42 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FadeLoader } from "react-spinners";
 
 const SignIn = () => {
   const [error, setError] = useState("");
+  const [hello, setHello] = useState(false);
   const router = useRouter();
+  const {data: session, status } = useSession();
+  const Router = useRouter();
+  console.log("st: ", status);
+  console.log("session: ", session);
+  
+  useEffect(() => {
+    if (status === "authenticated" || session) {
+      setHello(true)
+      Router.push("/");
+    }
+  }, [status, Router, session]);
 
   const handleSignIn = async () => {
     const res = await signIn("azure-ad", { redirect: false });
     if (res?.error) {
       setError("Authentication failed. Please use a @traxion.com account.");
     } else {
-      router.push("/");
+      router.push("/signin");
     }
   };
-  const { status } = useSession();
-  const Router = useRouter();
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      Router.push("/");
-    }
-  }, [status, Router]);
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
       {error && <p className="text-red-500 mb-4">{error}</p>}
+      {status === "loading" || hello ? <FadeLoader color="#08204A"/> :
       <button
         onClick={handleSignIn}
         className="bg-[#1e1e1e] text-[#dcdcaa] font-mono px-4 py-1 rounded-lg shadow-md border border-[#3c3c3c] hover:bg-[#252526] hover:!text-[#ffffff] transition duration-300 cursor-pointer"
       >
-        Sign in with Microsoft
-      </button>
+        Sign in with Microsoft"
+      </button>}
     </div>
   );
 };
